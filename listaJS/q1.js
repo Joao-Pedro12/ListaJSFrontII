17bd3abdfd33fad080d2fa624a981d37e5aed77e
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-11-09 19:27:34 -0300

    Add files via upload

---
+++ listaJS/q1.js
@@ -0,0 +1,17 @@
+let populacaoA = 80000; 
+let taxaCrescimentoA = 0.03;
+
+let populacaoB = 200000; 
+let taxaCrescimentoB = 0.015; 
+
+let anos = 0;
+
+while (populacaoA < populacaoB) {
+    populacaoA += populacaoA * taxaCrescimentoA;
+    populacaoB += populacaoB * taxaCrescimentoB;
+    anos++;
+}
+
+console.log(`Levará ${anos} anos para que a população do país A ultrapasse a população do país B.`);
+ 20 changes: 20 additions & 0 deletions20  
+ListaJS/q2.js
